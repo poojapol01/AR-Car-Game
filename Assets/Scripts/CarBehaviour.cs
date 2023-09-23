d901d68b3326861b3441f226af7871b3d517ec50
poojapol01 <polpooja05@gmail.com>
2023-09-24 00:03:05 +0530

    Added script to control the object's movement and rotation to follow a Reticle

---
+++ Assets/Scripts/CarBehaviour.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBehaviour : MonoBehaviour
+{
+    private ReticleBehaviour reticleBehaviour;
+    private float speed = 1.2f;
+
+    private void Update()
+    {
+        var trackingPosition = reticleBehaviour.transform.position;
+
+        if(Vector3.Distance(trackingPosition, transform.position) < 0.1)
+        {
+            return;
+        }
+
+        //Calculates a rotation (lookRotation) that points from the current object's position to the trackingPosition
+        var lookRotation = Quaternion.LookRotation(trackingPosition - transform.position);
+
+        //It smoothly adjusts the object's rotation towards the lookRotation, making the object face the Reticle
+        transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * 10f);
+
+        //It smoothly moves the object's position towards the trackingPosition at a speed determined by Speed
+        transform.position = Vector3.MoveTowards(transform.position, trackingPosition, Time.deltaTime * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
